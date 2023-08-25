a7becaae98e0259dfb316431ef2965bac4ab5531
John Francis <john9francis@gmail.com>
2023-08-25 09:54:39 -0600

    trying to make the dockerfilescript faster

--- dockerfile_script.sh
+++ dockerfile_script.sh
@@ -4,3 +4,11 @@
 # and the whole cmake project is copied into it.
 
 echo $PATH
+
+# Add conda-forge channel to find geant4 and other 
+# dependencies faster
+conda config --add channels conda-forge
+
+# Create conda geant4 environment
+# and install geant4 in it
+conda create -c conda-forge --name geant4env geant4
